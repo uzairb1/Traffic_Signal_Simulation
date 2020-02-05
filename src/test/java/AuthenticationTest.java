30820c9a3a876ca3d1e11268af0422cea75ed619
uzairb1 <39996413+uzairb1@users.noreply.github.com>
2020-02-06 00:45:47 +0100

    Update AuthenticationTest.java

--- src/test/java/AuthenticationTest.java
+++ src/test/java/AuthenticationTest.java
@@ -1,15 +1,5 @@
-import static org.junit.Assert.*;
-
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.junit.Test;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 public class AuthenticationTest {
 
 	@Test
