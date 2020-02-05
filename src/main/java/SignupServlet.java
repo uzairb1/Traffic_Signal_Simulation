bd3e09009fee26c6edc114719f089d8b978d88f4
uzairb1 <39996413+uzairb1@users.noreply.github.com>
2020-02-06 00:45:44 +0100

    Update SignupServlet.java

--- src/main/java/SignupServlet.java
+++ src/main/java/SignupServlet.java
@@ -15,21 +15,39 @@ public class SignupServlet extends HttpServlet {
 
 
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
 		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
+		try
+		{
+			request.getSession().setAttribute("role", request.getSession().getAttribute("role"));
+		request.getSession().getAttribute("logged").equals("true");
 		out.write("	<form name=\"signup\" action=\"PersonServlet\">"
-				+ "<input type=\"text\" name=\"addname\" placeholder=\"Name\">"
+				+ "<input type=\"text\" name=\"addname\" placeholder=\"*Name\">"
 				+ "<br><br>"
-				+ "<input type=\"password\" name=\"addpass\" placeholder=\"Password\">"
+				+ "<input type=\"password\" name=\"addpass\" placeholder=\"*Password\">"
 				+ "<br><br>"
 				+ "<input type=\"text\" name=\"FName\" placeholder=\"First Name\">"
 				+ "<br><br>"
-				+ "<input type=\"text\" name=\"addrole\" placeholder=\"Role\">"
-				+ "<br><br><input type=\"submit\" name=\"submit\" value=\"Submit\"></form>");
-
-		
+				+ "<input type=\"text\" name=\"addrole\" placeholder=\"*Role\">"
+				+ "<br><br><input type=\"submit\" name=\"submit\" value=\"Submit\"></form>"
+				+"<br/><p>Fields with \"*\" must not be left empty</p>");
+		out.flush();
+		}
+		catch(Exception e)
+		{
+			out.write("<body>" + 
+					"        <center>\r\n" + 
+					"            <form id=\"PersonServlet\" name=\"PersonServlet\" method=\"Get\" action=\"PersonServlet\">\r\n" + 
+					"                <p>You must login</p>\r\n" + 
+					"                <div id=\"loginBtn\">\r\n" + 
+					"                    <input id=\"btn\" type=\"submit\" value=\"Go Back\" />\r\n" + 
+					"                </div>\r\n" + 
+					"            </form>\r\n" + 
+					"        </center>\r\n" + 
+					"    </body>	");	
+		}
 	}
 	
 	
